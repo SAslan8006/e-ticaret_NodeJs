fbf54c9ee50ccfc6a2c62e3a067b4f84a92e2f93
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-22 11:17:18 +0300

    Login and Register (jwt, bcrypt and cors)

---
+++ controllers/auth.js
@@ -0,0 +1,56 @@
+const Auth = require('../models/auth.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await Auth.findOne({ email });
+    if (user) {
+      return res.status(500).json({ message: 'Bu email hesabı zaten bulunmaktadır..!' });
+    }
+    if (password.length < 6) {
+      return res.status(500).json({ message: 'Parolanız 6 karakterden küçük olmamalı...!' });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const newUser = await Auth.create({ username, email, password: passwordHash });
+
+    const userToken = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRESIN });
+
+    res.status(201).json({
+      status: 'Ok',
+      newUser,
+      userToken,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const login =async (req, res) => {
+    
+  try {
+    const { email, password } = req.body;
+    const user = await user.findOne({ email });
+    if (!user) { 
+        return res.status(500).json({ message: 'Böyle bir kullanıcı bulunamadı..!' });
+    }
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+        return res.status(500).json({ message: 'Parolanız yanlıştır..!' });
+    }
+    const token= jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRESIN });
+    res.status(200).json({
+        status: 'Ok',
+        user,
+        token,
+      });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+  
+module.exports = { register, login };
--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 const express = require('express');
-const cors =require('cors');
-const dotenv=require('dotenv');
-const db=require('./config/database.js');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db = require('./config/database.js');
+const Auth = require('./routes/auth.js');
 
 dotenv.config();
 
-const app=express();
+const app = express();
 app.use(cors());
 
-
-//static files middleware
-app.use(express.static('public'));
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
+// Router Register
+app.use('/',Auth);
 
-app.get('/',(req,res)=>{
-    res.json({message:"deneme deneme"});
+app.get('/', (req, res) => {
+  res.json({ message: 'deneme deneme' });
 });
 
 const port = process.env.PORT;
 
 db();
 
-
 app.listen(port, () => {
-    console.log(`Application running on port: ${port}`);
-  });
+  console.log(`Application running on port: ${port}`);
+});
