33bd1cc7679bb48efc423b3d70b5c34a087c5eb5
Süleyman Aslan <suleymanaslan.8006@gmail.com>
2023-03-22 14:27:45 +0300

    Proje middlewares login kontrollu

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getPost, createPost, getDetail, getUpdate, deletePost, searchPost } = require('../controllers/post.js');
+const auth = require('../models/auth.js');
 const router = express.Router();
 
 router.get('/getPost',getPost);
-router.post('/createPost',createPost);
+router.post('/createPost',auth,createPost);
 router.get('/getDetail/:id',getDetail);
-router.patch('/getUpdate/:id',getUpdate);
-router.delete('/deletePost/:id',deletePost);
+router.patch('/getUpdate/:id',auth,getUpdate);
+router.delete('/deletePost/:id',auth,deletePost);
 router.get('/searchPost',searchPost);
 
 module.exports=router
